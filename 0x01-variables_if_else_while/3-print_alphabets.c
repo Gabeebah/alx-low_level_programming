adb453cc309a9b9467bfa4e32e63e0dae2790505
gabeebah smith <gabeebahsmith063@gmail.com>
2023-04-24 07:10:15 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,24 @@
-#include <stdio.h> 
-
+#include <stdio.h>
 /**
-* main - Prints the alphabet. 
-*
-* Return: Always 0 (Success) 
-*/ 
+ * main - Entry point
+ * Description: 'print alphabets lowercase and uppercase'
+ * Return: always 0
+ */
 int main(void)
-{ 
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int i;
-
-for (i = 0; i < 52; i++)
 {
-putchar(alp[i];
-}
-putchar('\n');
-return (0);
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
 }

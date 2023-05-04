b2f9594e8683eb27976ef4b1e4f874c647c3ab44
gabeebah smith <gabeebahsmith063@gmail.com>
2023-05-04 12:24:19 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_topper - change all lowercase to uppercase
+ * @n: pointer
+ * return: n
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'a')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}

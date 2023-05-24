6d9973d3fc94c06d3ef45874fc17df803eec7822
gabeebah smith <gabeebahsmith063@gmail.com>
2023-05-24 06:06:09 -0700

    3-files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,9 +17,9 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	iny i = 0;
+	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op != *s))
+	while (ops[i].op != NULL &&  *(ops[i].f) *s)
 		i++;
 
 	return (ops[i].f);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,38 +8,38 @@ int op_mod(int a, int b);
 
 /**
 * op_add - Returns the sum of two numbers
-*@a: the first number
-@b: the second number 
+* @a: the first number
+* @b: the second number
 *
-*Return: the sum of a and b
+* Return: the sum of a and b
 */
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
 
 /**
-* op_sub - Returns the difference of two numbers
-@a: the first number
-@b: the second number
-*
-*return: the difference of a and b
-*/
+ * op_sub - Returns the difference between two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the difference of a and b
+ */
 int op_sub(int a, int b)
 {
-return (a - b)'
+	return (a - b);
 }
 
 /**
-* op_mul - Returns the product of two numbers
-@a: the first number
-@b: the second number
-*
-*return: the ptoduct of a and b
-*/
-int op_sub(int a, int b)
+ * op_mul - Returns the product of two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
 
 /**
@@ -51,7 +51,7 @@ return (a * b);
 */
 int op_div(int a, int b)
 {
-return (a / b);
+	return (a / b);
 }
 
 /**
@@ -63,5 +63,5 @@ return (a / b);
 */
 int op_mod(int a, int b)
 {
-return (a % b);
+	return (a % b);
 }
